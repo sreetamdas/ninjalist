9362394227d930e14f133669a614b477a3068c2f
vijethx <38877972+vijethx@users.noreply.github.com>
2022-02-17 15:18:12 +0530

    added dynamic routes

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../../styles/Ninjas.module.css";
 export const getStaticProps = async () => {
 	const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
@@ -19,11 +20,11 @@ const Ninjas = ({ ninjas }) => {
 			<div>
 				<h1>All Ninjas</h1>
 				{ninjas.map((ninja) => (
-					<div key={ninja.id}>
+					<Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
 						<a className={styles.single}>
 							<h3>{ninja.name}</h3>
 						</a>
-					</div>
+					</Link>
 				))}
 			</div>
 		</>
