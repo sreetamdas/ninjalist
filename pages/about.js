6ef85128906a81db06457311a753ad954e84818e
vijethx <38877972+vijethx@users.noreply.github.com>
2022-02-17 14:14:45 +0530

    added logo and metadata

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,23 @@
+import Head from "next/head";
 const About = () => {
 	return (
-		<div>
-			<h1>About Page</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
-				hic? Obcaecati rem necessitatibus omnis cupiditate.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
-				hic? Obcaecati rem necessitatibus omnis cupiditate.
-			</p>
-		</div>
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name='keywords' content='ninjas' />
+			</Head>
+			<div>
+				<h1>About Page</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+					hic? Obcaecati rem necessitatibus omnis cupiditate.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+					hic? Obcaecati rem necessitatibus omnis cupiditate.
+				</p>
+			</div>
+		</>
 	);
 };
 
