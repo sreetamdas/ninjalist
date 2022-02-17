ca877dfc9493ace8e22a732d03f93a1375066cf1
vijethx <38877972+vijethx@users.noreply.github.com>
2022-02-17 15:21:30 +0530

    final touches

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+
 const Navbar = () => {
 	return (
 		<nav>
 			<div className='logo'>
-				<h1>Ninja List</h1>
-				<Image src='/logo.png' width={128} height={77} alt='logo' />
+				<Image src='/logo.png' alt='site logo' width={128} height={77} />
 			</div>
-			<Link href='/'>Home</Link>
-			<Link href='/about'>About</Link>
-			<Link href='/ninjas'>Ninja Listing</Link>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='/about'>
+				<a>About</a>
+			</Link>
+			<Link href='/ninjas/'>
+				<a>Ninja Listing</a>
+			</Link>
 		</nav>
 	);
 };
